64690856634be595486d6127518468f3b048b892
Chizobaobiakor <lucyobiakor@gmail.com>
2023-06-21 16:43:45 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,40 +6,39 @@
  */
 void print_times_table(int n)
 {
-    int i, j, k;
+	int i, j, k;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                k = j * i;
+	if (n >= 0 && n <= 15)
+	{
+	for (i = 0; i <= n; i++)
+	{
+	for (j = 0; j <= n; j++)
+	{
+	k = j * i;
 
-                if (j == 0)
-                {
-                    _putchar(k + '0');
-                }
-                else
-                {
-                    _putchar(' ');
+	if (j == 0)
+	{
+	_putchar(k + '0');
+	}
+	else
+	{
+	_putchar(' ');
 
-                    if (k < 10)
-                        _putchar(' ');
-                    else if (k >= 100)
-                        _putchar((k / 100) + '0');
+	if (k < 10)
+	_putchar(' ');
+	else if (k >= 100)
+	_putchar((k / 100) + '0');
 
-                    if (k >= 10)
-                        _putchar(((k / 10) % 10) + '0');
+	if (k >= 10)
+	_putchar(((k / 10) % 10) + '0');
 
-                    _putchar((k % 10) + '0');
-                }
+	_putchar((k % 10) + '0');
+	}
 
-                if (j < n)
-                    _putchar(',');
-            }
-
-            _putchar('\n');
-        }
-    }
+	if (j < n)
+	_putchar(',');
+	}
+	_putchar('\n');
+	}
+	}
 }
