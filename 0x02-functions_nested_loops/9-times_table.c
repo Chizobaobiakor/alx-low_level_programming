397cdfab1d139e36e65de9ac4e7b425aae6b903a
Chizobaobiakor <lucyobiakor@gmail.com>
2023-06-21 15:30:51 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,6 +6,7 @@
 void times_table(void)
 {
 	int i, j, k;
+	
 	for (i = 0; i < 10; i++)
 	{
 	for (j = 0; j < 10; j++)
@@ -17,7 +18,7 @@ void times_table(void)
 	}
 
 	if (k < 10 && j != 0)
-	{			
+	{
 	_putchar(',');
 	_putchar(' ');
 	_putchar(' ');
@@ -27,7 +28,7 @@ void times_table(void)
 	{
 	_putchar(',');
 	_putchar(' ');
-	_putchar((k / 10) + '0'); 
+	_putchar((k / 10) + '0');
 	_putchar((k % 10) + '0');
 	}
 	}
