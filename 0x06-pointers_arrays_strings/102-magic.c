01d9774e431480f5cc5b6ff9af3ee8fe7b9ea32b
Chizobaobiakor <lucyobiakor@gmail.com>
2023-06-29 15:41:06 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a[5] = {98, 198, 298, 398, 498};
+	int *p = &a[2];
+
+	/* Your code goes here */
+	*(p + 0) = 98;
+
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
