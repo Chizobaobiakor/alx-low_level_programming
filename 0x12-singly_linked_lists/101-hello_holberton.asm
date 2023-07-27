4c118a4c70130ba10bea339b5112facf28adc5b8
Chizobaobiakor <lucyobiakor@gmail.com>
2023-07-27 15:24:51 -0700

    holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,9 @@
-section .data
-hello db "Hello, Holberton",0
-format db "%s",10,0
-
-section .text
-global main
-
-extern printf
-
+global   main
+extern    printf
 main:
-; Prepare the arguments for printf
-mov rdi, format
-mov rsi, hello
-
-; Call printf
-call printf
-
-; Exit the program
-mov rax, 60         ; syscall number for exit
-xor rdi, rdi        ; exit code 0
-syscall
+mov   edi, format
+xor   eax, eax
+call  printf
+mov   eax, 0
+ret
+format: db `Hello, Holberton\n`,0
