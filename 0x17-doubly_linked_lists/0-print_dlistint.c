e1c9b007da45a7689c9a8469adb5edbc0b494359
Chizobaobiakor <lucyobiakor@gmail.com>
2024-01-11 07:43:11 -0800

    surviving of recloned cloned repo again

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,30 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Printing a double linked list
- * @h: Pointer to the head of the list
- * Return: Size of the list
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-	int len = 0;
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
 
 	while (h != NULL)
 	{
-		len++;
 		printf("%d\n", h->n);
+		count++;
 		h = h->next;
 	}
-	return (len);
+
+	return (count);
 }
